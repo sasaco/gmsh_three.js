9029e10c9539cf2d8d5d741198bef7a427125e4d
sasaco <sasaicco@gmail.com>
2022-03-01 08:33:00 +0900

    del

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,16 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  code: string = "console.log(100)";
-
-  // フローティングウィンドウの位置
-  public dragPosition = { x: 0, y: 0 };
-
-  public changePosition() {
-    this.dragPosition = {
-      x: this.dragPosition.x,
-      y: this.dragPosition.y,
-    };
-  }
 
 }
